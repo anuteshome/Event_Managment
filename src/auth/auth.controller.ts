8b14c82056ba47f0a2aab1146d428d1d9cdfcc89
@Nu73424545 <ananyateshome2@gmail.com>
2026-08-08 16:56:12 +0300

    feat: Run migration and down volumn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Body,Post } from '@nestjs/common';
+import { signupDto } from './dto/signup.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    @Post('signup')
+    signup(@Body() body:signupDto){
+        return body;
+    }
+
+
+
+}
