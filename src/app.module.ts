979cba0fa58f30d60dcb5abcdf6d0c90c421ae39
@Nu73424545 <ananyateshome2@gmail.com>
2026-08-08 16:07:36 +0300

    feat: Run migration and down volumn

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import MikroOrmConfig from './mikro-orm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MikroOrmModule.forRoot(MikroOrmConfig)
+    MikroOrmModule.forRoot(MikroOrmConfig),
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
