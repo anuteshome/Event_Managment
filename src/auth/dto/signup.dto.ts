d0683abf0cca3f48a9a6ffa34f3dd29b75b774d3
@Nu73424545 <ananyateshome2@gmail.com>
2026-08-08 16:50:16 +0300

    feat: Run migration and down volumn

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,2 +1,17 @@
 import {IsEmail,IsNotEmpty,IsString,MinLength} from 'class-validator';
 
+export class signupDto{
+
+    @IsEmail()
+    email!:string;
+
+    @IsString()
+    @MinLength(6)
+    password!:string;
+
+    @IsString()
+    firstName!:string;
+
+    @IsString()
+    lastName!:string;
+}
