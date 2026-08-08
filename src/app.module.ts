4b046295ad7a8b97991a6f505fac37ba90cd6287
@Nu73424545 <ananyateshome2@gmail.com>
2026-08-08 15:28:39 +0300

    feat: Creating user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 
-import MikroOrmConfig from './micro-orm.config';
+import MikroOrmConfig from './mikro-orm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
