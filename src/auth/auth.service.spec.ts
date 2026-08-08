f4b5a589a58446935f2f7fdd122465d12961c01e
@Nu73424545 <ananyateshome2@gmail.com>
2026-08-09 00:08:40 +0300

    Chore: Setup ci cd pipeline updates 1

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { jest } from '@jest/globals';
+import { getRepositoryToken } from '@mikro-orm/nestjs';
+import { EntityManager } from '@mikro-orm/core';
 import { AuthService } from './auth.service';
+import { User } from '../user/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            findOne: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+        {
+          provide: EntityManager,
+          useValue: {
+            persistAndFlush: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,49 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { EntityRepository, EntityManager } from '@mikro-orm/core';
+import bcrypt from 'bcryptjs';
+
+import { User } from '../user/user.entity';
+import { SignupDto } from './dto/signup.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: EntityRepository<User>,
+    private readonly em: EntityManager,
+  ) {}
 
-// import { ConflictException, Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@mikro-orm/nestjs';
-// import { EntityRepository } from '@mikro-orm/postgresql';
-// import * as bcrypt from 'bcryptjs';
+  async signup(dto: SignupDto) {
+    const existingUser = await this.userRepository.findOne({
+      email: dto.email,
+    });
 
-// import { User } from '../user/user.entity';
-// import { SignupDto } from './dto/signup.dto';
+    if (existingUser) {
+      throw new ConflictException('Email already exists');
+    }
 
-// @Injectable()
-// export class AuthService {
-//   constructor(
-//     @InjectRepository(User)
-//     private readonly userRepository: EntityRepository<User>,
-//   ) {}
+    const hashedPassword: string = await bcrypt.hash(dto.password, 10);
 
-//   async signup(dto: SignupDto) {
-//     const existingUser = await this.userRepository.findOne({
-//       email: dto.email,
-//     });
+    const user = this.userRepository.create({
+      email: dto.email,
+      password: hashedPassword,
+      firstName: dto.firstName,
+      lastName: dto.lastName,
+      createdAt: new Date(),
+    });
 
-//     if (existingUser) {
-//       throw new ConflictException('Email already exists');
-//     }
+    this.em.persist(user);
+    await this.em.flush();
 
-//     const hashedPassword = await bcrypt.hash(dto.password, 10);
-
-//     const user = this.userRepository.create({
-//       email: dto.email,
-//       password: hashedPassword,
-//       firstName: dto.firstName,
-//       lastName: dto.lastName,
-//       createdAt: new Date(),
-//     });
-
-//     await this.userRepository.getEntityManager().persistAndFlush(user);
-
-//     return {
-//       id: user.id,
-//       email: user.email,
-//       firstName: user.firstName,
-//       lastName: user.lastName,
-//     };
-//   }
-// }
+    return {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
+  }
+}
