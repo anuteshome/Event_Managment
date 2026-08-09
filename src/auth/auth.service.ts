b9ab85991ebf74219844c6ba72401bffed6c1304
@Nu73424545 <ananyateshome2@gmail.com>
2026-08-10 00:06:59 +0300

    fix:auth Service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,46 +1,33 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository, EntityManager } from '@mikro-orm/core';
-import bcrypt from 'bcryptjs';
+import { Injectable,ConflictException } from "@nestjs/common";
+import {EntityRepository,EntityManager} from "@mikro-orm/core";
+import {InjectRepository} from "@mikro-orm/nestjs";
+import bcrypt from "bcryptjs";
 
-import { User } from '../user/user.entity';
-import { SignupDto } from './dto/signup.dto';
+import {User} from "../user/user.entity";
+import {SignupDto} from"./dto/signup.dto";
+
 
 @Injectable()
-export class AuthService {
+export class AuthService{
+  
   constructor(
-    @InjectRepository(User)
-    private readonly userRepository: EntityRepository<User>,
-    private readonly em: EntityManager,
-  ) {}
+@InjectRepository(User)
+private readonly userRepository:EntityRepository<User>,
+private readonly em:EntityManager
+  ){}
 
-  async signup(dto: SignupDto) {
-    const existingUser = await this.userRepository.findOne({
-      email: dto.email,
+  async signup (dto:SignupDto){
+    const existingUser= await this.userRepository.findOne({
+      email:dto.email
     });
+      
+     if(existingUser){
+      throw new ConflictException("")
+     }
+     
 
-    if (existingUser) {
-      throw new ConflictException('Email already exists');
-    }
 
-    const hashedPassword: string = await bcrypt.hash(dto.password, 10);
 
-    const user = this.userRepository.create({
-      email: dto.email,
-      password: hashedPassword,
-      firstName: dto.firstName,
-      lastName: dto.lastName,
-      createdAt: new Date(),
-    });
-
-    this.em.persist(user);
-    await this.em.flush();
-
-    return {
-      id: user.id,
-      email: user.email,
-      firstName: user.firstName,
-      lastName: user.lastName,
-    };
   }
+
 }
