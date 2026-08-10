b9614399c41d238cb9089c39f4f99af01dc0da09
@Nu73424545 <ananyateshome2@gmail.com>
2026-08-10 20:20:51 +0300

    feat/velkap_1_investor_lookup_and_linking

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+    const config = new DocumentBuilder()
+    .setTitle('Event Management API')
+    .setDescription('API documentation for the Event Management project')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
