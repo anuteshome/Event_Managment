6155968cbb069e64b5291d8b2a52318642aedd10
@Nu73424545 <ananyateshome2@gmail.com>
2026-08-10 20:05:59 +0300

    fix: Save Postgress

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Body, Post } from '@nestjs/common';
 import { SignupDto } from './dto/signup.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  // constructor({private readonly a})
+  constructor(private readonly authService:AuthService){}
 
   @Post('signup')
-  signup(@Body() body: SignupDto): SignupDto {
-    return body;
+  signup(@Body() body: SignupDto) {
+    return this.authService.signup(body);
+
   }
 }
+
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,52 +1,51 @@
-import { Injectable,ConflictException } from "@nestjs/common";
-import {EntityRepository} from "@mikro-orm/core";
-import {EntityManager} from "@mikro-orm/postgresql";
-import {InjectRepository} from "@mikro-orm/nestjs";
-import bcrypt from "bcryptjs";
+import { ConflictException, Injectable } from '@nestjs/common';
+import { EntityRepository } from '@mikro-orm/core';
+import { EntityManager } from '@mikro-orm/postgresql';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import bcrypt from 'bcryptjs';
 
-import {User} from "../user/user.entity";
-import {SignupDto} from"./dto/signup.dto";
-
+import { User } from '../user/user.entity';
+import { SignupDto } from './dto/signup.dto';
 
 @Injectable()
-export class AuthService{
-  
+export class AuthService {
   constructor(
-@InjectRepository(User)
-private readonly userRepository:EntityRepository<User>,
-private readonly em:EntityManager
-  ){}
+    @InjectRepository(User)
+    private readonly userRepository: EntityRepository<User>,
 
-  async signup (dto:SignupDto){
-    const existingUser= await this.userRepository.findOne({
-      email:dto.email
+    private readonly em: EntityManager,
+  ) {}
+
+  async signup(dto: SignupDto) {
+    const existingUser = await this.userRepository.findOne({
+      email: dto.email,
     });
-      
-     if(existingUser){
-      throw new ConflictException("Email Already Used");
-     }
-     
-     const hashedPassword:string = await bcrypt.hash(dto.password,10);
-     
+
+    if (existingUser) {
+      throw new ConflictException('Email Already Used');
+    }
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
     const user = this.userRepository.create({
-      email:dto.email,
-      password:hashedPassword,
-      firstName:dto.firstName,
-      lastName:dto.lastName,
-    createdAt:new Date()
+      email: dto.email,
+      password: hashedPassword,
+      firstName: dto.firstName,
+      lastName: dto.lastName,
+      createdAt: new Date(),
     });
 
-    this.em.persist(user),
-    await this.em.flush();
+  this.em.persist(user);
+await this.em.flush();
 
-     return {
-      id:user.id,
-      email:user.email,
-      firstName:user.firstName,
-      lastName:user.lastName,
-      createdAt:user.createdAt
+    console.log('USER SAVED:', user.id, user.email);
 
-     }
+    return {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      createdAt: user.createdAt,
+    };
   }
-
 }
