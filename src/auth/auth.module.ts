f295fc932d81f23bf9af8f09944fbf474b04b512
@Nu73424545 <ananyateshome2@gmail.com>
2026-08-11 11:01:34 +0300

    fix:Setup Jwt Module in the auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/user.entity';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import {JwtModule} from '@nestjs/jwt';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([User])],
+  imports: [
+    MikroOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret:'this-is-some-sceret-key-for-jwt-token',
+      signOptions:{
+      expiresIn:"1h",
+      }
+    })
+  
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
